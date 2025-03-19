640f59e0e17cef310eeee5545b058ebcca8a69d5
Ibrahim Cisse <42039693+ibraheemcisse@users.noreply.github.com>
2025-03-19 08:39:33 +0000

    shell

---
+++ .github/workflows/script.sh
@@ -0,0 +1,7 @@
+    #/bin/sh
+        
+    sudo apt-get install cowsay -y 
+    cowsay -f dragon "run for ur life rawrrrrrrrrr" >> dragon.txt 
+    run: grep -i "." dragon.txt 
+    cat dragon.txt 
+    ls -ltra
